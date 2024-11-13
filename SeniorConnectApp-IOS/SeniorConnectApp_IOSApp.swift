c178be97cc183bb643affd4e9158df1060806fe2
ayazhankadessova <kadessovaayazhan@gmail.com>
2024-11-13 15:03:43 +0800

    Initial Commit

---
+++ SeniorConnectApp-IOS/SeniorConnectApp_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeniorConnectApp_IOSApp.swift
+//  SeniorConnectApp-IOS
+//
+//  Created by Аяжан on 13/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct SeniorConnectApp_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
